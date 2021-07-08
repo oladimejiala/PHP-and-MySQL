fe2f749f6d20f28d3a69ae56901518137b3f5c2d
oladimejiala <oladimejialabim@gmail.com>
2021-07-08 03:30:48 +0100

    Index.php file to create a database template for projects

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Neglected Tropical Diseases DataBase</title>
+</head>
+<body>
+ echo "<p>Hello Michael </p>"
+<!-- <header>
+<img src="" alt="NTDs Database logo">
+    <h1>ARNTDs</h1>
+    <p>African Research Network for Negelected Tropical Diseases</p>
+    <p>.....towards an NTD free Africa</p>
+</header>
+
+
+
+    <p>&copy; 2021 ARNTD | All Rights Researved | Developed by AMTECH</p> -->
+</body>
+</html>
